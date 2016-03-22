fc2340ef657ec8e26e41d33a979072c02b998a1c
igor_peresunko <marazmatic77@gmail.com>
2016-03-22 07:24:38 +0200

    added inputs

--- lab2/rendering/main.py
+++ lab2/rendering/main.py
@@ -52,13 +52,18 @@ class SaveImage(object):
 
 
 class CalculationDistance(object):
-    def __init__(self, number_of_layers, number_of_neurons_in_layer):
+    def __init__(self, number_of_inputs, number_of_outputs, number_of_layers, number_of_neurons_in_layer):
+        self.number_of_inputs = number_of_inputs
+        self.number_of_outputs = number_of_outputs
         self.number_of_layers = number_of_layers
         self.number_of_neurons_in_layer = number_of_neurons_in_layer
+
         self.radius = 20
         self.height = 640
         self.width = 1200
         self.central_coordinates = {}  # the coordinates of centers of neurons
+        self.inputs_coordinates = {}
+        self.outputs_coordinates = {}
 
     def distance_between_layers(self):
         return self.width / self.number_of_layers
@@ -76,17 +81,14 @@ class CalculationDistance(object):
         The calculation of the centers of the neurons
         (the distance between neurons, the distance between layers)
         """
-        coord_y = 0
-        coord_x = 0
+        coord_y = 320
+        coord_x = -640
         distance_between_l = self.distance_between_layers()
         distance_between_n = self.distance_between_neurons()
 
-        for index_l, layer in enumerate(xrange(1, self.number_of_layers + 1)):
+        for layer in xrange(1, self.number_of_layers + 1):
             layer_data = []
-            if index_l:
-                coord_x += distance_between_l
-            else:
-                coord_x = -640  # starting coordinates X
+            coord_x += distance_between_l
 
             for index_n, neuron in enumerate(xrange(1, self.number_of_neurons_in_layer + 1)):
 
@@ -100,29 +102,49 @@ class CalculationDistance(object):
             self.central_coordinates[layer] = layer_data
 
         pprint(self.central_coordinates)
+        self.calculate_outputs()
 
+    def calculate_inputs(self):
+        distance_betwen_n = self.distance_between_neurons()
+        coords = []
+        coord_x = -640
+        coord_y = 320
+
+        for index, n in enumerate(xrange(1, self.number_of_inputs + 1)):
+            if index:
+                coord_y -= distance_betwen_n
+
+            coords.append((coord_x, coord_y))
+
+        self.inputs_coordinates['input'] = coords
+        pprint(self.inputs_coordinates)
+        self.calculate_center_coordinates()
+
+    def calculate_outputs(self):
+        print 'outputs'
 
 if __name__ == '__main__':
-    draw = RenderingNeuralNetwork()
+    # draw = RenderingNeuralNetwork()
 
-    def darw_link(target, layer):
-        for neuron in layer:
-            draw.draw_link(target, neuron)
+    # def darw_link(target, layer):
+    #     for neuron in layer:
+    #         draw.draw_link(target, neuron)
 
-    calc = CalculationDistance(3, 2)
-    calc.calculate_center_coordinates()
-    for layer in sorted(calc.central_coordinates.keys()):
-        for n in calc.central_coordinates[layer]:
-            draw.draw_neuron(n, calc.radius)
-
-    for index, layer in enumerate(sorted(calc.central_coordinates.keys()), 1):
-        for n in calc.central_coordinates[layer]:
-            if index != len(calc.central_coordinates.keys()):
-                darw_link(n, calc.central_coordinates[layer + 1])
+    calc = CalculationDistance(2, 2, 3, 3)
+    calc.calculate_inputs()
+    # calc.calculate_center_coordinates()
+    # for layer in sorted(calc.central_coordinates.keys()):
+    #     for n in calc.central_coordinates[layer]:
+    #         draw.draw_neuron(n, calc.radius)
+    #
+    # for index, layer in enumerate(sorted(calc.central_coordinates.keys()), 1):
+    #     for n in calc.central_coordinates[layer]:
+    #         if index != len(calc.central_coordinates.keys()):
+    #             darw_link(n, calc.central_coordinates[layer + 1])
 
     # s = SaveImage()
     # s.save()
-    tr.mainloop()
+    # tr.mainloop()
 
     # TODO: layers paint in different colors
-    # Todo: add input and output parameters
+    # TODO: add output parameters
